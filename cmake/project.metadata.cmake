c5ea7c85cdbefd97d11397c1f8853e481c41d718
milerius <rmscastle@gmail.com>
2021-04-08 10:30:55 +0200

    feat(cmake): add CMakeLists.txt and project.metadata.cmake

---
+++ cmake/project.metadata.cmake
@@ -0,0 +1,22 @@
+## Project Name / Binary Name
+set(PROJECT_BINARY_NAME "MyApp")
+
+##! Project Name
+set(PROJECT_NAME "Onouris")
+
+##! Project Version
+set(PROJECT_VERSION_MAJOR 0)
+set(PROJECT_VERSION_MINOR 0)
+set(PROJECT_VERSION_PATCH 1)
+set(PROJECT_VERSION 0.0.1)
+
+##! Project METADATA Json file
+set(PROJECT_JSON_METADATA "{
+    \"project_binary_name\": \"${PROJECT_BINARY_NAME}\",
+    \"project_version\": \"${PROJECT_VERSION}\"
+}")
+
+macro(generate_project_metafiles)
+    message(STATUS "Generating project.metadata.json with:\n${PROJECT_JSON_METADATA}")
+    file(WRITE ${CMAKE_SOURCE_DIR}/project.metadata.json ${PROJECT_JSON_METADATA})
+endmacro()
